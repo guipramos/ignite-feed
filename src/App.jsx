0df59b458c1062084d878e34c26a151495c4dcb9
Guilherme Ramos <gui.pramos@hotmail.com>
2022-08-23 16:12:54 -0300

    Finalizando o sidebar da pagina e adicionando a biblioteca de icone phosphoricons

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
-import { Header } from './components/Header';
+import { Header } from '../src/components/Header/Header';
 import './global.css';
+
+import styles from './App.module.css';
+import { Sidebar } from './components/Sidebar/Sidebar';
 
 function App() {
 
   return (
-    <Header />
+    <>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <div><p>asdasdadadada</p></div>
+          <div><p>asdasdadadada</p></div>
+        </main>
+      </div>
+    </>
   )
 }
 
---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,21 @@
+import {PencilLine} from 'phosphor-react'
+import styles from "./Sidebar.module.css";
+
+export function Sidebar(){
+    return (
+        <aside className={styles.sidebar}>
+            <img className={styles.cover} src="https://images.unsplash.com/photo-1603468620905-8de7d86b781e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=40" />
+            <div className={styles.profile}>
+                <img src="https://avatars.githubusercontent.com/u/21373296?v=4" />
+                <strong>Guilherme Ramos</strong>
+                <span>Web Developer</span>
+            </div>
+            <footer>
+                <a href="#">
+                    <PencilLine size={20} />
+                    Editar seu perfil
+                </a>
+            </footer>
+        </aside>
+    );
+}
